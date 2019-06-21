22e5b9fb367be9066a53814b4fc2f207750905a9
chr <chenhr1130@163.com>
2019-06-21 18:43:46 +0800

    完成后台数据传入前台

--- src/main/java/edu/team/controller/UserController.java
+++ src/main/java/edu/team/controller/UserController.java
@@ -2,23 +2,46 @@ package edu.team.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.team.entity.User;
 import edu.team.service.UserService;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 项目初始control层示例
  */
-@RestController
+@Controller
 public class UserController {
 
     @Autowired
-    UserService userService;
+    private UserService userService;
 
     @RequestMapping("/test")
     public User test() {
         return userService.select();
+    }
+
+    @RequestMapping("/m")
+    public ModelAndView testMode() {
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        //调用service方法得到用户列表
+//        List<User> users = userService.findUser();
+        //将得到的用户列表内容添加到ModelAndView中
+        modelAndView.addObject("id",12);
+        //设置响应的jsp视图
+        modelAndView.setViewName("index");
+        return modelAndView;
+
+//        return new ModelAndView("index", "id", 31);
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "abc";
     }
 }
