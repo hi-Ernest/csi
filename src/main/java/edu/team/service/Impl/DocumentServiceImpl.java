52675836a23d35ff4e45bc7bdcc9507d1e3fc2c0
代利文 <450326779@qq.com>
2019-06-26 14:53:06 +0800

    文件模块主要功能完成

--- src/main/java/edu/team/service/Impl/DocumentServiceImpl.java
+++ src/main/java/edu/team/service/Impl/DocumentServiceImpl.java
@@ -3,6 +3,7 @@ package edu.team.service.Impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -27,11 +28,30 @@ public class DocumentServiceImpl implements DocumentService {
 
     @Override
     public List<Document> findDocumentFromTitle(String title) {
-        return null;
+        return documentDao.findDocumentFromTitle(title);
+    }
+
+    @Override
+    public Document findDocumentFromId(Integer id) {
+        return documentDao.findDocumentFromId(id);
     }
 
     @Override
     public String deleteDocumentFromId(Integer id) {
+        Document document = documentDao.findDocumentFromId(id);
+        String filePath = "D://CsiFile//";
+        String fileName = document.getFileName();
+        File file = new File(filePath + fileName);
+        if (!file.exists()) {
+            System.out.println("删除文件失败:" + fileName + "不存在！");
+            return "FAIL";
+        } else {
+            if (file.isFile()) {
+                if (!deleteFile(filePath + fileName)) {
+                    return "FAIL";
+                }
+            }
+        }
         try {
             documentDao.deleteDocumentFromId(id);
             return "SUCCESS";
@@ -55,5 +75,28 @@ public class DocumentServiceImpl implements DocumentService {
     @Override
     public String updateDocument(String title, String createData, String userId, String remake, String id) {
         return null;
+    }
+
+    /**
+     * 删除单个文件
+     *
+     * @param fileName 要删除的文件的文件名
+     * @return 单个文件删除成功返回true，否则返回false
+     */
+    public static boolean deleteFile(String fileName) {
+        File file = new File(fileName);
+        // 如果文件路径所对应的文件存在，并且是一个文件，则直接删除
+        if (file.exists() && file.isFile()) {
+            if (file.delete()) {
+                System.out.println("删除单个文件" + fileName + "成功！");
+                return true;
+            } else {
+                System.out.println("删除单个文件" + fileName + "失败！");
+                return false;
+            }
+        } else {
+            System.out.println("删除单个文件失败：" + fileName + "不存在！");
+            return false;
+        }
     }
 }
